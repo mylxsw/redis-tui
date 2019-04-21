ce95c6904c8660501e57f1732639d73e32dd160d
管宜尧 <guanyiyao@yunsom.com>
2019-04-21 19:26:00 +0800

    init

---
+++ Makefile
@@ -0,0 +1,17 @@
+Version := $(shell date "+%Y%m%d%H%M")
+GitCommit := $(shell git rev-parse HEAD)
+LDFLAGS := "-s -w -X main.Version=$(Version) -X main.GitCommit=$(GitCommit)"
+
+run: build 
+	./bin/redis-gli
+
+build:
+	go build -race -ldflags $(LDFLAGS) -o bin/redis-gli *.go
+
+release:
+	CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags $(LDFLAGS) -o release/redis-gli-darwin *.go
+	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags $(LDFLAGS) -o release/redis-gli-win.exe *.go
+	CGO_ENABLED=0 GGOOS=linux GOARCH=amd64 go build -ldflags $(LDFLAGS) -o release/redis-gli-linux *.go
+
+.PHONY: run build release
+
---
+++ redis.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/redis.v5"
+)
+
+type RedisClient interface {
+	Keys(pattern string) *redis.StringSliceCmd
+	Type(key string) *redis.StatusCmd
+	TTL(key string) *redis.DurationCmd
+	Get(key string) *redis.StringCmd
+	LRange(key string, start, stop int64) *redis.StringSliceCmd
+	SMembers(key string) *redis.StringSliceCmd
+	ZRangeWithScores(key string, start, stop int64) *redis.ZSliceCmd
+	HKeys(key string) *redis.StringSliceCmd
+	HGet(key, field string) *redis.StringCmd
+}
+
+// NewRedisClient create a new redis client which wraps single or cluster client
+func NewRedisClient(config Config) RedisClient {
+	if config.Cluster {
+		options := &redis.ClusterOptions{
+			Addrs:    []string{fmt.Sprintf("%s:%d", config.Host, config.Port),},
+			Password: config.Password,
+		}
+
+		return redis.NewClusterClient(options)
+	}
+
+	options := &redis.Options{
+		Addr: fmt.Sprintf("%s:%d", config.Host, config.Port),
+		DB:   config.DB,
+	}
+
+	return redis.NewClient(options)
+}
+
